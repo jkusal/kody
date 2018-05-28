c424a47da326973c9cb0e807e80a8f630930eb15
jkusal <juskusal@gmail.com>
2018-05-28 09:41:04 +0200

    python

---
+++ python/mlotek.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+# ctrl shift P
+
+
+def main(args):
+    liczba = random.randint(1, 10)
+    print("Wylosowano:", liczba)
+
+    odp = input("Podaj liczbę od 1 do 10: ")
+    print("Podaleś liczbę:", odp)
+
+    if liczba == int(odp):
+        print("Zgadłeś!")
+    else:
+        print("Spróbuj jeszcze raz!")
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
