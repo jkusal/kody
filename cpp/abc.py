805396776d341db3ef96929fa8ce2cbb1a6521c2
jkusal <juskusal@gmail.com>
2018-09-18 14:19:45 +0200

    abc

--- cpp/abc.py
+++ cpp/abc.py
@@ -1,19 +1,29 @@
+#
+#Program pobiera 3 liczby od użytkownika , a następnie wyświetla największą
+#
+def maks(a, b, c):
+    m=None
+    
+    if a >= b & a >= c:
+        print(a, "jest największe")
+    elif b >= a & b >= c:
+        print(b, "jest największe")
+    else: 
+        print(c, "jest największe")
+        
+    return m;
+
+
 
 def main(args):
-    a = int(input("Podaj a: "))
-    print(a)
-    
-    b = int(input("Podaj b: "))
-    print(b)
-    
-    if a > b:
-        print(a, "jest większe od", b)
-        
-    if a < b:
-        print(b, "jest większe od", a)
-    
-    if a == b:
-        print(a, "jest równe", b)
+    assert(maks(3, 2, 1) == 3)
+    assert(maks(1, 3, 1) == 3)
+    assert(maks(1, 2, 3) == 3)
+    assert(maks(1, 1, 3) == 3)
+    assert(maks(3, 1, 1) == 3)
+    assert(maks(3, 3, 1) == 3)
+    assert(maks(1, 3, 3) == 3)
+    assert(maks(3, 3, 3) == 3)
         
     return 0
 
