a97eda81deb72569d95b2b0bcd5efb534ca9cd1f
jkusal <juskusal@gmail.com>
2018-10-02 13:40:59 +0200

    ćwiczenia pętle

--- python/petle_cw4.py
+++ python/petle_cw4.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+
 def main(args):
-    
-    a = 10
-    b = 100
-		
-    if a <= b:
-        for liczba in range(a, b):
-			if liczba % 3 == 0:
-                print(liczba)
-   
-         
-    return 0
+	
+	for a in range(10, 100):
+		if a %2 ==0 and a %3 == 0:
+			print (a)
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
