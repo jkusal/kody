9b762267424677569f9d14dd2e4ffc8c5e738c37
jkusal <juskusal@gmail.com>
2018-11-20 14:20:26 +0100

    żółw

--- python/kwadraty.py
+++ python/kwadraty.py
@@ -3,10 +3,25 @@
 
 import turtle
 
-def rysujKwadrat(zolw, bok):
+def rysujKwadrat(zolw, bok, ile):
+    # for i in range(4):
+    zolw.forward(bok)
+    zolw.right(90)
+    if ile > 0:
+        rysujKwadrat(zolw, bok, ile-1)    
+    
+def rysujKwadrat2(zolw, bok):
     for i in range(4):
         zolw.forward(bok)
         zolw.right(90)
+    if bok > 0:
+        rysujKwadrat2(zolw, bok-10)
+        
+def rysuj(zolw, bok, kat, warunek):
+    zolw.forward(bok)
+    zolw.right(kat)
+    if kat < warunek:
+        rysuj(zolw, bok, kat, warunek)
     
 def main(args):
     turtle.title("Kwadraty")
@@ -14,8 +29,15 @@ def main(args):
     
     zolw = turtle.Turtle()
     zolw.color('green')
+    zolw.speed(0)
+    zolw.pensize(2)
     
-    rysujKwadrat(zolw, 100)
+    # rysujKwadrat(zolw, 100, 4)
+    # zolw.begin_fill()
+    # rysujKwadrat2(zolw, 100)
+    # zolw.end_fill()
+    
+    rysuj(zolw, 100, 60, 180)
     
     turtle.done()
     
