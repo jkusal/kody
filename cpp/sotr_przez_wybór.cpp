f8ee54a471aa594316d9d682d41a4bf487e77a84
jkusal <juskusal@gmail.com>
2018-12-19 11:40:00 +0100

    sort

--- cpp/sotr_przez_wybór.cpp
+++ cpp/sotr_przez_wybór.cpp
@@ -28,23 +28,26 @@ void drukuj(int tab[], int rozmiar)
 }
 
 int sort(int tab[], int n) {
-    int i = 0;
-    for (i; i < n; i++) {
+    int k;
+    for (int i = 0; i < n; i++) {
         k = i;
         for (int j = i + 1; j < n; j++) {
-            if
-            tab[i] = tab[k];
+            if (tab[j] < tab[k]) {
+                k = j;
+            }
         }
-        cout << tab[];
+        cout << tab[k];
     }
     return 0;
 }
 
 int main(int argc, char **argv)
 {
+    int rozmiar = 20;
     int tablica[rozmiar];  // statyczna deklaracja tablicy
     los(tablica, rozmiar);
     drukuj(tablica, rozmiar);
+    cout << endl;
     sort(tablica, rozmiar);
     
 	return 0;
