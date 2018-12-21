1f535aa4e092770ec5d2f704490b2aadecce3e44
jkusal <juskusal@gmail.com>
2018-12-21 07:32:54 +0100

    sort

--- aplikacje/quiz/quiz.py
+++ aplikacje/quiz/quiz.py
@@ -25,9 +25,14 @@ def after_request(response):
 def hello():
     return "<h1>Witaj na serwerze!</h2><h2>Aplikacja quiz</h2>"
     
+@app.route("/lista")
+def lista():
+    pytania = Pytanie.select()
+    return render_template('lista.html', query = pytania)
+    
 @app.route("/quiz")
 def quiz():
-    pytania = Pytanie.select()
+    pytania = Pytanie.select().join(Odpowiedz).distinct()
     return render_template('quiz.html', query = pytania)
 
 @app.route("/klasa")
