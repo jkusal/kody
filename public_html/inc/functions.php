79cd527b022671af63c759f03453d92cc9fc4e0f
jkusal <juskusal@gmail.com>
2019-02-22 08:30:40 +0100

    php

---
+++ public_html/inc/functions.php
@@ -0,0 +1,25 @@
+<?php
+$pages = array(
+    'witam' => 'Witamy',
+    'baza' => 'Baza',
+    'formularz' => 'Formularz',
+    'login' => 'Logowanie',
+);
+
+function get_menu(&id) {
+    global $pages;
+    foreach ($pages as $p => $t) {
+        echo '
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="?id='.$p.'">'.$t.'</a>
+            </li>';
+    }
+}
+
+function get_page_content($id) {
+    if (file_exists($id.'.html'))
+        include($id.'.html');
+    else
+        echo '<p>Brak takiej strony!</p>'
+}
+?>
