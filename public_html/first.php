6640bb45600739c180a9753332b90ae155aa63ea
jkusal <juskusal@gmail.com>
2019-03-01 08:42:48 +0100

    php

---
+++ public_html/first.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<!-- first.php -->
+
+<head>
+<meta charset="utf-8" />
+<title>Test PHP</title>
+<meta name="generator" content="Geany 1.32" />
+<?php
+
+?>
+
+</head>
+
+<body>
+<?php
+    echo("<h1>Witaj w php!</h1>");
+ ?>
+
+</body>
+
+</html>
---
+++ public_html/skroty.php
@@ -0,0 +1,10 @@
+<?php
+    // funkcje skrótu
+    $tekst = 'cokolwiek';
+    echo hash('sha1', $tekst);
+    echo "\n";
+    echo hash('sha256', $tekst);
+    echo "\n";
+    $haslo = 'bardzotajnehaslo';
+    echo hash('sha256', $haslo);
+?>
